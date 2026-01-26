6108323944f57732d1c1125b06fe8050e93d05e7
leogit015 <leonardodevs2@gmail.com>
2026-01-25 22:18:37 -0300

    Criação da classe SuiteGeral

---
+++ src/test/java/tests/SuiteGeral.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        // Liste aqui as classes que você quer rodar
+        CadastroTest.class,
+        ConectoresTest.class,
+        GrupoCarregamentoTest.class,
+        PontoCarregamentoTest.class
+})
+public class SuiteGeral {
+    // Essa classe fica vazia mesmo, ela serve só pra agrupar os testes
+}
